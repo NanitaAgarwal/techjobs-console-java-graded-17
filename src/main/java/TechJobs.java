4bca0ba78a5239f3e126d1ce50374c2534f1b433
NanitaAgarwal <nanita.agarwal@gmail.com>
2023-09-07 19:55:30 -0500

    Assignment # 1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -118,7 +118,7 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
         if (someJobs.size() == 0){
-            System.out.println("No Results\r");
+            System.out.print("No Results");
         } else {
             for (HashMap<String, String> job : someJobs) {
                 System.out.println("\n*****");
